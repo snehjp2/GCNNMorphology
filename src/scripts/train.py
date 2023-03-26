a7cd06619815f928c3704b72b321c09f5b494762
snehjp2 <snehjp2@gmail.com>
2023-03-25 20:46:25 -0400

    fixing seeds in train.py

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -23,7 +23,16 @@ from e2cnn import nn as e2cnn_nn
 from models import model_dict
 from dataset import Galaxy10DECals, Galaxy10DECalsTest
 from tqdm import tqdm
+import random
 
+
+def set_all_seeds(num):
+    random.seed(num)
+    np.random.seed(num)
+    torch.manual_seed(num)
+    
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(num)
 
 
 def train_model(model, train_dataloader, test_dataloader, optimizer, scheduler = None, epochs=100, device='cuda', save_dir='checkpoints', early_stopping_patience=10, report_interval=5):
@@ -95,8 +104,6 @@ def train_model(model, train_dataloader, test_dataloader, optimizer, scheduler =
 
     torch.save(model.state_dict(), os.path.join(save_dir, "final_model.pt"))
     
-    return best_test_acc, losses[-1]
-
     # Plot loss vs. training step graph
     plt.figure(figsize=(10, 5))
     plt.plot(steps, losses)
@@ -104,6 +111,8 @@ def train_model(model, train_dataloader, test_dataloader, optimizer, scheduler =
     plt.ylabel('Loss')
     plt.title('Loss vs. Training Steps')
     plt.savefig(os.path.join(save_dir, "loss_vs_training_steps.png"), bbox_inches='tight')
+    
+    return best_test_acc, losses[-1]
 
 # We don't need gradients during evaluation.
 @torch.no_grad()
@@ -125,7 +134,7 @@ def evaluate(eval_loader: DataLoader, model: nn.Module):
     print("Correct answer in {:.1f}% of cases.".format(accuracy * 100))
     
 @torch.no_grad()
-def plot_confusion_matrix(test_loader: DataLoader, save_dir: str, model: nn.Module, device = 'cuda'):
+def plot_confusion_matrix(data_loader: DataLoader, save_dir: str, model: nn.Module, device = 'cuda'):
     best_model = model
     best_model_path = f'{save_dir}/best_model.pt'
     
@@ -133,8 +142,7 @@ def plot_confusion_matrix(test_loader: DataLoader, save_dir: str, model: nn.Modu
     y_pred = []
     y_true = []
 
-    device = 'cpu'
-    for batch in test_loader:
+    for batch in data_loader:
             inputs, labels = batch[0].to(device), batch[1].to(device)
             output = best_model(inputs) # Feed Network
             pred_labels = torch.argmax(output, dim=-1).cpu().numpy()
@@ -154,7 +162,6 @@ def plot_confusion_matrix(test_loader: DataLoader, save_dir: str, model: nn.Modu
     sn.heatmap(df_cm, annot=True)
     plt.title(f'Confusion Matrix')
     plt.savefig(os.path.join(save_dir, "confusion_matrix.png"), bbox_inches='tight')
-
 
 
 @torch.no_grad()
@@ -199,21 +206,21 @@ def main(config):
     timestr = time.strftime("%Y%m%d-%H%M%S")
     save_dir = config['save_dir'] + config['model'] + '_' + timestr
     best_acc, final_loss = train_model(model, train_dataloader, val_dataloader, optimizer, scheduler, epochs=config['parameters']['epochs'], device=device, save_dir=save_dir,early_stopping_patience=config['parameters']['early_stopping'], report_interval=config['parameters']['report_interval'])
+
+    # print("Loading test dataset!")
+    # start = time.time()
+    # test_dataset = Galaxy10DECalsTest(config['dataset']['test'], transform)
+    # end = time.time()
+    # print(f"Test dataset loaded in {end - start} s")
+    # test_dataloader = DataLoader(test_dataset, batch_size = config['parameters']['batch_size'], shuffle=False)
+    
+    plot_confusion_matrix(data_loader = val_dataloader, save_dir = save_dir, model = model)
+    
     config['best_acc'] = best_acc
     config['final_loss'] = final_loss
     file = open(f'{save_dir}/config.yaml',"w")
     yaml.dump(config, file)
     file.close()
-    
-    print("Loading test dataset!")
-    start = time.time()
-    test_dataset = Galaxy10DECalsTest(config['dataset']['test'], transform)
-    end = time.time()
-    print(f"Test dataset loaded in {end - start} s")
-    
-    test_dataloader = DataLoader(test_dataset, batch_size = config['parameters']['batch_size'], shuffle=False)
-    plot_confusion_matrix(test_loader = test_dataloader, save_dir = save_dir, model = model)
-    
     
 if __name__ == '__main__':
 
@@ -221,6 +228,8 @@ if __name__ == '__main__':
         device = torch.device('cuda')
     else:
         device = torch.device('cpu') 
+        
+    set_all_seeds(42)
 
     parser = argparse.ArgumentParser(description = 'Train the models')
     parser.add_argument('--config', metavar = 'config', required=True,
