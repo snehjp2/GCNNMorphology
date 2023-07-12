2b2c03e15433983d3e12cf1932ce0f42c6ff686f
snehjp2 <snehjp2@gmail.com>
2023-07-12 14:30:30 -0500

    changes to dataset loading

--- src/scripts/dataset.py
+++ src/scripts/dataset.py
@@ -6,6 +6,7 @@ import numpy as np
 from torch.utils.data import Dataset
 import torchvision.transforms as T
 import matplotlib.pyplot as plt
+import time
 from torchvision import transforms
 
 
@@ -17,7 +18,6 @@ class Galaxy10DECals(Dataset):
     """
     def __init__(self,dataset_path : str, transform = None) :
         self.dataset_path = dataset_path
-        #self.dataset = None
         self.transform = transform
         with h5py.File(self.dataset_path,"r") as f:
             self.img = f['images'][()]
@@ -42,47 +42,49 @@ class Galaxy10DECalsTest(Dataset):
     Args:
         dataset_path (string) : path to h5 file
     """
-    def __init__(self,dataset_path : str, transform = None) :
+    def __init__(self,dataset_path : str, transform = None):
         self.dataset_path = dataset_path
-        self.dataset = None
         self.transform = transform
         with h5py.File(self.dataset_path,"r") as f:
+            self.img = f['images'][()]
+            self.label = f['labels'][()]
+            self.angle = f['angles'][()]
+            self.redshift = f['redshifts'][()]
             self.length = len(f['labels'][()])
+            
 
     def __getitem__(self, idx):
 
-        if self.dataset is None:
-            self.dataset = h5py.File(self.dataset_path,"r")
-
-        img = self.dataset['images'][idx]
-        label = torch.tensor(self.dataset['labels'][idx],dtype=torch.long)
-        angle = torch.tensor(self.dataset['angles'][idx],dtype=torch.long)
-        redshift = torch.tensor(self.dataset['redshifts'][idx],dtype=torch.float)
+        img = self.img[idx]
+        label = torch.tensor(self.label[idx],dtype=torch.long)
+        angle = torch.tensor(self.angle[idx],dtype=torch.long)
+        redshift = torch.tensor(self.redshift[idx],dtype=torch.float)
         
         if self.transform:
             img = self.transform(img)
-        return img, label , angle, redshift
+        return img, label, angle, redshift
 
     def __len__(self):
         return self.length
 
 if __name__ == '__main__':
-    print(sys.argv[1])
+    
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.RandomRotation(180),
-        transforms.CenterCrop(180),
-        transforms.Resize(256),
+        transforms.Resize(255),
         transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
         transforms.RandomHorizontalFlip(p=0.3),
         transforms.RandomVerticalFlip(p=0.3),
         transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
     ])
-    dataset = Galaxy10DECals(sys.argv[1],transform=transform)
-    #dataset = Galaxy10DECalsTest(sys.argv[1],transform=transform)
-    print(len(dataset))
-    img , label = dataset[12342]
-    print(img.shape)
-    print(label)
-    plt.imshow(img.permute(1,2,0).numpy())
-    plt.show()
+    start_time = time.time()
+    dataset = Galaxy10DECalsTest(sys.argv[1],transform=transform)
+    end_time = time.time()
+    print("Time taken to load dataset: ", end_time-start_time)
+    
+    start_time = time.time()
+    for i in range(len(dataset)):
+        img ,label, _, _ = dataset[i]
+    end_time = time.time()
+    print("Time taken to load all images: ", end_time-start_time)
